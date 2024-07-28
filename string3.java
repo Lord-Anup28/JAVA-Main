c208d1cbd5d7d52fb9d4b96e8e56b09ccfa30ab8
Lord-Anup28 <anupbouri7@gmail.com>
2024-07-28 15:30:46 +0000

    concacatate  ion of string

---
+++ string3.java
@@ -0,0 +1,8 @@
+//string is immutable means we can't updatae or change the value of the string but we can concatenate
+public class string3{
+    public static void main(String[] args){
+        String st="Hello";
+        st=st+"e";
+        System.out.println(st);
+    }
+}
